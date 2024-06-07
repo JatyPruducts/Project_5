744d466907029c276980c3645df64a68587bf83a
Михаил <134938010+JatyPruducts@users.noreply.github.com>
2024-06-07 12:39:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_5
+{
+    public class Store
+    {
+        public int NumGoods { get; protected set; }
+
+        public Store(int num)
+        {
+            if (num < 0) throw new ExceptionGoods();
+            NumGoods = num;
+        }
+
+        public void Removal(int num)
+        {
+            try
+            {
+                NumGoods -= num;
+                if (NumGoods < 0) throw new ExceptionGoods();
+            }
+            catch (ExceptionGoods e)
+            {
+                NumGoods = 0;
+                throw e;
+            }
+            finally
+            {
+                Console.WriteLine($"На складе осталось {NumGoods} товара");
+            }
+        }
+    }
+}
---
+++ main.cs
@@ -0,0 +1,18 @@
+namespace Project_5
+{
+    internal class main
+    {
+        static void Main(string[] args)
+        {
+            var s = 5;
+            var s1 = new Store(23);
+            var s2 = new Store(5);
+            var s3 = new Store(0);
+            
+            s1.Removal(s);
+            s2.Removal(s);
+            s3.Removal(s);
+        }
+    }  
+}
+
